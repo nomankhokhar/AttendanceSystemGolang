c69158d74f4771229077d6d8ef34d01756ebf830
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-09-14 15:14:10 +0500

    Backend Server is Ready

---
+++ backend/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+
+	r.GET("/", helloPoint)
+	r.Run(":8080")
+}
+
+func helloPoint(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Attendance is Working",
+	})
+}
