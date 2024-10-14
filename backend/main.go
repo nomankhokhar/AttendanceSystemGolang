6b186e3cf814fce8f2dd238b5aa62f825a658890
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-10-14 13:51:47 -0700

    fix: add CORS in the Backend golang code

--- backend/main.go
+++ backend/main.go
@@ -13,6 +13,20 @@ import (
 	"AttendanceSystem/db"
 )
 
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-ClusterQueueLenght, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
 
 func main() {
 	err := godotenv.Load()
@@ -20,12 +34,12 @@ func main() {
 		log.Fatalf("Error loading .env file")
 	}
 	mongoClient, err := db.InitDB()
-	
+
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
 		return
 	}
-	
+
 	defer func() {
 		if err := mongoClient.Disconnect(context.Background()); err != nil {
 			log.Printf("Error disconnecting from database: %v", err)
@@ -34,18 +48,20 @@ func main() {
 
 	r := gin.Default()
 
+	r.Use(CORS())
+
 	r.GET("/ping", helloPoint)
 
 	// User Handlers
 	r.POST("/signup", userController.SignUp)
 	r.POST("/login", userController.Login)
-	
+
 	r.POST("/forgot-password", userController.ForgotPassword)
 	r.POST("/reset-password", userController.ResetPassword)
 
 	// User Attendance Handlers
-	r.POST("/attendance/insert", attendanceController.InsertAttendance) 
-	r.GET("/attendance/getattendance", attendanceController.GetAttendanceByEmail) 
+	r.POST("/attendance/insert", attendanceController.InsertAttendance)
+	r.GET("/attendance/getattendance", attendanceController.GetAttendanceByEmail)
 	r.DELETE("/attendance/deleteattendance", attendanceController.DeleteAttendanceByEmailAndID)
 	r.POST("/attendance/updateattendance", attendanceController.UpdateAttendanceByEmailAndID)
 	if err := r.Run(":8080"); err != nil {
